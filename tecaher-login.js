54a4f8bf54d9a50bbc771941541d767bf9d41422
Ryumamogi <ryuma.mogis@gmail.com>
2026-03-04 16:59:03 +0900

    Add files via upload

---
+++ tecaher-login.js
@@ -0,0 +1,20 @@
+let answer = "";
+// "!103!" が入力されるまでループ
+while (answer !== "!103!") {
+    answer = prompt("パスワードを入力してください");
+
+    // キャンセルされた場合はループを抜ける
+    if (answer === null) {
+        alert("キャンセルされました");
+        break;
+    }
+
+    // 正解した場合
+    if (answer === "!103!") {
+        alert("ログインに成功しました、ホームページに移動します");
+        // index.htmlへ自動ワープ
+        window.location.href = "../管理者/edu.html"; 
+    } else {
+        alert("違います。もう一度入力してください");
+    }
+}
